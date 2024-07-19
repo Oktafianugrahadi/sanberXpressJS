36eec9becd499fa6b3bce4881677fa29ec6ab2e2
oktafianugrahadi <oktafianugrahadi95@gmail.com>
2024-07-19 15:32:47 +0700

    Pengumpulan Tugas 5

---
+++ tugas5-express-intro/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// Middleware untuk log setiap permintaan
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+// Melayani file statis dari direktori "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route untuk root path
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "Success fetch message",
+//     data: "Hello World!",
+//   });
+// });
+
+// Route untuk /user
+app.get("/user", (req, res) => {
+  res.json({
+    message: "Success fetch user",
+    data: {
+      id: 1,
+      name: "Budi",
+      username: "budidu",
+      email: "budidu@mail.com",
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at <http://localhost>:${port}`);
+});
